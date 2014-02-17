9ad2e93b1dfc2f3ad38e9d1ef5fe7de685a150f8
JumpMaster <kevin@cooper.uk.com>
2014-02-17 08:58:57 -0800

    Commit 170214
    Codes stored on phone and sync'd on first launch
    basic secret error checking
    2 themes
    Improved config page

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,21 +1,31 @@
-var otp_count = localStorage.getItem("otp_count");
+var otp_count = parseInt(localStorage.getItem("otp_count"));
+var theme = parseInt(localStorage.getItem("theme"));
+var timezoneOffset = new Date().getTimezoneOffset();
+
+if (!otp_count)
+	otp_count = 0;
+
+if (!theme)
+	theme = 0;
 
 Pebble.addEventListener("ready",
 							function(e) {
 								console.log("JavaScript app ready and running!");
-								sendTimezoneToWatch();
-								if (!isNaN(otp_count))
-									otp_count = 0;
+								
+								//otp_count = 4;
+								//localStorage.setItem("otp_count", otp_count);
+								//localStorage.removeItem("secret4");
+								//localStorage.removeItem("secret6");
+								//localStorage.removeItem("secret7");
+
+								// Send timezone and keycount to watch
+								Pebble.sendAppMessage({"key_count":otp_count, "theme":theme, "timezone":timezoneOffset});
+								
+								console.log("otp_count="+otp_count);
+								console.log("theme="+theme);
+								console.log("timezoneOffset="+timezoneOffset);
 							}
 						);
-
-function sendTimezoneToWatch() {
-	// Get the number of seconds to add to convert localtime to utc
-	var offsetMinutes = new Date().getTimezoneOffset();
-	// Send it to the watch
-	console.log(offsetMinutes);
-	Pebble.sendAppMessage({"timezone":offsetMinutes});
-}
 
 function sendKeyToWatch(secret) {
 	Pebble.sendAppMessage({"transmit_key":secret});
@@ -25,43 +35,59 @@ function sendKeyToWatch(secret) {
 Pebble.addEventListener("appmessage",
 							function(e) {
 								console.log("Message Recieved");
-								if (e.payload.timezone) {
-									sendTimezoneToWatch();
-								}
 								if (e.payload.request_key) {
 									console.log(e.payload.request_key);
-									sendKeyToWatch(localStorage.getItem("secret_pair"+e.payload.request_key));
-									otp_count += 1;
+									sendKeyToWatch(localStorage.getItem("secret_pair"+(e.payload.request_key-1)));
 								}
-								if (e.payload.key_count) {
-									console.log("Key count request");
-									Pebble.sendAppMessage({"send_key_count":otp_count});
+								else {
+									console.log(e.payload);
 								}
 							});
 
 Pebble.addEventListener('showConfiguration', function(e) {
-	Pebble.openURL('http://192.168.0.115');
-	//Pebble.openURL('http://htmlpreview.github.io/?https://github.com/JumpMaster/PebbleAuth/blob/master/configuration.html');
+	Pebble.openURL('http://192.168.4.17');
+	//Pebble.openURL('http://htmlpreview.github.io/?https://github.com/JumpMaster/PebbleAuth/blob/master/html/configuration.html');
 });
 
 Pebble.addEventListener("webviewclosed",
 							function(e) {
-								console.log("Configuration window returned: " + e.response);
-								if(otp_count < 8) {
-									console.log("Uploading codes...");
-									var configuration = JSON.parse(e.response);
-									var secret = configuration.secret;
-									secret = secret.replace(/0/g,"O").replace(/1/g, "I").replace(/\+/g, '').replace(/\s/g, '').toUpperCase();
-									var secretPair = configuration.label + ":" + secret;
-									console.log("Configuration window returned: " + secretPair);
-									sendKeyToWatch(secretPair);
-									console.log('secret_pair'+otp_count);
-									localStorage.setItem('secret_pair'+otp_count,secretPair);
-									otp_count++;
-									localStorage.setItem('otp_count',otp_count);
-								} else {
-									console.log("Too many codes...");
+								var configuration = JSON.parse(e.response);
+								var config ={};
+								
+								if(!isNaN(configuration.theme) && configuration.theme != theme) {
+									theme = configuration.theme;
+									localStorage.setItem("theme",theme);
+									config.theme = theme;
+									console.log("Theme changed...");
 								}
 								
+								if(otp_count < 8) {
+									if(configuration.label.length > 0 && configuration.secret.length > 0) {
+										console.log("Uploading codes...");
+										var secret = configuration.secret;
+										secret = secret.replace(/0/g,"O").replace(/1/g, "I").replace(/\+/g, '').replace(/\s/g, '').toUpperCase();
+										
+										// Check if secret exists
+										var blnKeyExists = false;
+										for (var i=0;i<otp_count;i++) {
+											var savedSecret = localStorage.getItem('secret_pair'+i);
+											if (savedSecret !== null && savedSecret.indexOf(secret) != -1)
+												blnKeyExists = true;
+										}
+										
+										if (!blnKeyExists) {
+											var secretPair = configuration.label + ":" + secret;
+											console.log("Uploading new key:"+secretPair);
+											localStorage.setItem('secret_pair'+otp_count,secretPair);
+											otp_count++;
+											localStorage.setItem("otp_count",otp_count);
+											config.transmit_key = secretPair;
+										} else
+											console.log("Key exists, ignoring");
+									}
+								} else
+									console.log("Too many codes...");
+
+								Pebble.sendAppMessage(config);
 							}
 						);
--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include "pebble.h"
 #include "google-authenticator.c"
 #define MAX_OTP 8
-#define MAX_LABEL_LENGTH 6
-#define MAX_KEY_LENGTH 32
+#define MAX_LABEL_LENGTH 7 // 6 + termination
+#define MAX_KEY_LENGTH 33 // 32 + termination
 
 Window *window;
 static TextLayer *countdown_layer;
@@ -13,6 +13,9 @@ static ActionBarLayer *action_bar_layer;
 static GRect text_pin_rect;
 static GRect text_label_rect;
 
+static GColor bg_color;
+static GColor fg_color;
+
 static GFont font_BITWISE_32;
 static GFont font_ORBITRON_28;
 
@@ -20,59 +23,95 @@ static GFont font_ORBITRON_28;
 static GBitmap *image_icon_yes;
 static GBitmap *image_icon_no;
 
+bool loading_complete = false;
 bool refresh_data = false;
 bool finish_refresh = true;
+bool requesting_codes = false;
 
 int timezone_offset = 0;
+unsigned int theme = 0;  // 0 = Dark, 1 = Light
+
+unsigned int phone_otp_count = 0;
+unsigned int otp_count = 0;
+unsigned int otp_selected = 0;
+unsigned int otp_default = 3;
+
+char otp_labels[MAX_OTP][MAX_LABEL_LENGTH];
+char otp_keys[MAX_OTP][MAX_KEY_LENGTH];
+
+char label_override_text[MAX_LABEL_LENGTH] = "NO";
+char pin_override_text[MAX_LABEL_LENGTH] = "SECRET";
 
 enum {
-	PS_TIMEZONE_KEY = 0x0,
-	PS_SECRET = 0x1,
+	PS_TIMEZONE_KEY,
+	PS_THEME,
+	PS_SECRET
 };
-
-int otp_count = 0;
-int otpselected = 0;
-char otplabels[MAX_OTP][MAX_LABEL_LENGTH+1];
-char otpkeys[MAX_OTP][MAX_KEY_LENGTH+1];
-
-char label_override_text[MAX_LABEL_LENGTH+1] = "NO";
-char pin_override_text[MAX_LABEL_LENGTH+1] = "SECRET";
 
 enum {
-	JS_KEY_COUNT = 0x0,
-	JS_REQUEST_KEY = 0x1,
-	JS_TRANSMIT_KEY = 0x2,
-	JS_TIMEZONE = 0x3,
-	JS_DISPLAY_MESSAGE = 0x4
+	JS_KEY_COUNT,
+	JS_REQUEST_KEY,
+	JS_TRANSMIT_KEY,
+	JS_TIMEZONE,
+	JS_DISPLAY_MESSAGE,
+	JS_THEME
 };
+
+void window_config_provider(Window *window);
+void actionbar_config_provider(void *context);
 
 void expand_key(char *inputString)
 {
 	bool colonFound = false;
 	int outputChar = 0;
 	
+	char otp_key[MAX_KEY_LENGTH];
+	char otp_label[MAX_LABEL_LENGTH];
+	
 	for(unsigned int i = 0; i < strlen(inputString); i++) {
 		if (inputString[i] == ':') {
-			otplabels[otp_count][outputChar] = '\0';
+			otp_label[outputChar] = '\0';
 			colonFound = true;
 			outputChar = 0;
 		} else {
 			if (colonFound) 
-				otpkeys[otp_count][outputChar] = inputString[i]; 
+				otp_key[outputChar] = inputString[i]; 
 			else
-				otplabels[otp_count][outputChar] = inputString[i];
+				otp_label[outputChar] = inputString[i];
 			
 			outputChar++;
 		}
 	}
-	otpkeys[otp_count][outputChar] = '\0';
+	otp_key[outputChar] = '\0';
 	
-	pin_override_text[0] = '\0';
-	label_override_text[0] = '\0';
-
-	otp_count++;
-	otpselected = otp_count-1;
-	refresh_data = true;
+	bool key_found = false;
+	for(unsigned int i = 0; i < otp_count; i++) {
+		if (strcmp(otp_key, otp_keys[i]) == 0)
+			key_found = true;
+	}
+			
+	if (!key_found)
+	{
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "Adding Code");
+		strcpy(otp_keys[otp_count], otp_key);
+		strcpy(otp_labels[otp_count], otp_label);
+		
+		pin_override_text[0] = '\0';
+		label_override_text[0] = '\0';
+	
+		otp_count++;
+		if (loading_complete) {
+			otp_selected = otp_count-1;
+			refresh_data = true;
+		}
+	} else {
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "Code exists...skipping");
+	}
+	
+	if (requesting_codes && otp_count == phone_otp_count) {
+		requesting_codes = false;
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "FINISHED REQUESTING");
+	}
 }
 
 void on_animation_stopped(Animation *anim, bool finished, void *context)
@@ -129,7 +168,7 @@ static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
 			if(strlen(label_override_text))
 				text_layer_set_text(text_label_layer, label_override_text);
 			else
-				text_layer_set_text(text_label_layer, otplabels[otpselected]);
+				text_layer_set_text(text_label_layer, otp_labels[otp_selected]);
 			GRect start = text_label_rect;
 			start.origin.x = 144;
 			animate_layer(text_layer_get_layer(text_label_layer), AnimationCurveEaseOut, &start, &text_label_rect, 300, 0);
@@ -138,7 +177,7 @@ static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
 		if(strlen(pin_override_text))
 			text_layer_set_text(text_pin_layer, pin_override_text);
 		else
-			text_layer_set_text(text_pin_layer, generateCode(otpkeys[otpselected], timezone_offset));
+			text_layer_set_text(text_pin_layer, generateCode(otp_keys[otp_selected], timezone_offset));
 			
 		
 		GRect start = text_pin_rect;
@@ -162,25 +201,26 @@ static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
 
 void up_single_click_handler(ClickRecognizerRef recognizer, void *context) {
 
-	if (otpselected == (otp_count-1))
-		otpselected = 0;
+	if (otp_selected == (otp_count-1))
+		otp_selected = 0;
 	else
-		otpselected++;
+		otp_selected++;
 
 	refresh_data = true;
 }
 
 void down_single_click_handler(ClickRecognizerRef recognizer, void *context) {
-	if (otpselected == 0)
-		otpselected = (otp_count-1);
+	if (otp_selected == 0)
+		otp_selected = (otp_count-1);
 	else
-		otpselected--;
+		otp_selected--;
 
 	refresh_data = true;
 }
 
-void select_single_click_handler(ClickRecognizerRef recognizer, void *context) {
-	Tuplet request_tuple = TupletInteger(JS_REQUEST_KEY, otp_count);
+void request_code(int code_id) {
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Requesting code: %d", code_id);
+	Tuplet request_tuple = TupletInteger(JS_REQUEST_KEY, code_id);
 	
 	DictionaryIterator *iter;
 	app_message_outbox_begin(&iter);
@@ -195,10 +235,31 @@ void select_single_click_handler(ClickRecognizerRef recognizer, void *context) {
 	app_message_outbox_send();
 }
 
+void select_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+	request_code((int) otp_count+1);
+}
+
 void select_long_click_handler(ClickRecognizerRef recognizer, void *context) {
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "Adding ActionBar");
+	
+	// Initialize the action bar:
+	action_bar_layer = action_bar_layer_create();
+	action_bar_layer_set_background_color(action_bar_layer, fg_color);
+	action_bar_layer_set_click_config_provider(action_bar_layer, actionbar_config_provider);
+	action_bar_layer_set_icon(action_bar_layer, BUTTON_ID_UP, image_icon_yes);
+    action_bar_layer_set_icon(action_bar_layer, BUTTON_ID_DOWN, image_icon_no);
 	action_bar_layer_add_to_window(action_bar_layer, window);
+	
 	strcpy(pin_override_text, "DROP");
+	refresh_data = true;
+}
+
+void actionbar_up_click_handler(ClickRecognizerRef recognizer, void *context) {
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Removing ActionBar");
+	pin_override_text[0] = '\0';
+	action_bar_layer_remove_from_window(action_bar_layer);
+	action_bar_layer_destroy(action_bar_layer);
+	window_set_click_config_provider(window, (ClickConfigProvider) window_config_provider);
 	refresh_data = true;
 }
 
@@ -209,14 +270,6 @@ void window_config_provider(Window *window) {
 	window_long_click_subscribe(BUTTON_ID_SELECT, 750, select_long_click_handler, NULL);
 }
 
-void actionbar_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "Removing ActionBar");
-	pin_override_text[0] = '\0';
-	action_bar_layer_remove_from_window(action_bar_layer);
-	window_set_click_config_provider(window, (ClickConfigProvider) window_config_provider);
-	refresh_data = true;
-}
-
 void actionbar_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_DOWN, actionbar_up_click_handler);
   //window_single_click_subscribe(BUTTON_ID_UP, (ClickHandler) my_previous_click_handler);
@@ -225,6 +278,12 @@ void actionbar_config_provider(void *context) {
 void out_sent_handler(DictionaryIterator *sent, void *context) {
 	// outgoing message was delivered
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "Outgoing Message Delivered");
+	
+	
+	if (requesting_codes) {
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "REQUESTING ANOTHER!");
+		request_code(otp_count+1);
+	}
 }
 
 
@@ -233,14 +292,42 @@ void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, voi
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "Outgoing Message Failed");
 }
 
+
+void set_theme() { 
+	if (theme) {
+		bg_color = GColorWhite;
+		fg_color = GColorBlack;
+	} else {
+		bg_color = GColorBlack;
+		fg_color = GColorWhite;
+	}
+	
+	window_set_background_color(window, bg_color);
+	text_layer_set_background_color(countdown_layer, fg_color);
+	text_layer_set_text_color(text_label_layer, fg_color);
+	text_layer_set_text_color(text_pin_layer, fg_color);
+}
+
 static void in_received_handler(DictionaryIterator *iter, void *context) {
 	// Check for fields you expect to receive
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "Message Recieved");
 	
+	Tuple *key_count_tuple = dict_find(iter, JS_KEY_COUNT);
 	Tuple *key_tuple = dict_find(iter, JS_TRANSMIT_KEY);
 	Tuple *timezone_tuple = dict_find(iter, JS_TIMEZONE);
+	Tuple *theme_tuple = dict_find(iter, JS_THEME);
 	
 	// Act on the found fields received
+	if (key_count_tuple) {
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "Key count from phone: %d", key_count_tuple->value->int16);
+		phone_otp_count = key_count_tuple->value->int16;
+		
+		if (otp_count == 0 && phone_otp_count > 0) {
+			APP_LOG(APP_LOG_LEVEL_DEBUG, "REQUESTING CODES!!!");
+			requesting_codes = true;
+			request_code(otp_count+1);
+		}
+	}
 	if (key_tuple) {
 		char key_value[40];
 		memcpy(key_value, key_tuple->value->cstring, key_tuple->length);
@@ -256,7 +343,14 @@ static void in_received_handler(DictionaryIterator *iter, void *context) {
 			timezone_offset = tz_offset;
 			refresh_data = true;
 		}
-		APP_LOG(APP_LOG_LEVEL_DEBUG, "Int: %d", timezone_offset);
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "Timezone Offset: %d", timezone_offset);
+	}
+	
+	if (theme_tuple)
+	{
+		theme = theme_tuple->value->int16;
+		set_theme();
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "Theme: %d", theme);
 	}
 }
 
@@ -267,6 +361,7 @@ void in_dropped_handler(AppMessageResult reason, void *context) {
 
 void load_persistent_data() {
 	timezone_offset = persist_exists(PS_TIMEZONE_KEY) ? persist_read_int(PS_TIMEZONE_KEY) : 0;
+	theme = persist_exists(PS_THEME) ? persist_read_int(PS_THEME) : 0;
 	
 	if (persist_exists(PS_SECRET))
 	{
@@ -281,18 +376,24 @@ void load_persistent_data() {
 				break;
 		}
 	}
+	
+	if (otp_default < otp_count)
+		otp_selected = otp_default;
+
+	loading_complete = true;
 }
 
 void save_persistent_data() {
 	persist_write_int(PS_TIMEZONE_KEY, timezone_offset);
+	persist_write_int(PS_THEME, theme);
 	
 	char buff[40];
 	for(int i = 0; i < MAX_OTP; i++) {
 		buff[0] = '\0';
-		if(strlen(otplabels[i])) {
-			strcat(buff,otplabels[i]);
+		if(strlen(otp_labels[i])) {
+			strcat(buff,otp_labels[i]);
 			strcat(buff,":");
-			strcat(buff,otpkeys[i]);
+			strcat(buff,otp_keys[i]);
 			APP_LOG(APP_LOG_LEVEL_DEBUG, buff);
 			persist_write_string(PS_SECRET+i, buff);
 		}
@@ -301,21 +402,20 @@ void save_persistent_data() {
 
 void handle_init(void) {
 	load_persistent_data();
+	
 	window = window_create();
-	window_set_background_color(window, GColorBlack);
+	window_set_click_config_provider(window, (ClickConfigProvider) window_config_provider);
 	
 	Layer *window_layer = window_get_root_layer(window);
 	GRect bounds = layer_get_frame(window_layer);
 
 	countdown_layer = text_layer_create(GRect(0, bounds.size.h-10, 0, 10));
-	text_layer_set_background_color(countdown_layer, GColorWhite);
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(countdown_layer));
 	
 	text_label_rect = GRect(0, 30, bounds.size.w, 125);
 	GRect text_label_start_rect  = text_label_rect;
 	text_label_start_rect.origin.x = 144;
 	text_label_layer = text_layer_create(text_label_start_rect);
-	text_layer_set_text_color(text_label_layer, GColorWhite);
 	text_layer_set_background_color(text_label_layer, GColorClear);
 	text_layer_set_text_alignment(text_label_layer, GTextAlignmentLeft);
 	font_ORBITRON_28 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ORBITRON_28));
@@ -326,16 +426,15 @@ void handle_init(void) {
 	GRect text_pin_start_rect = text_pin_rect;
 	text_pin_start_rect.origin.x = 144;
 	text_pin_layer = text_layer_create(text_pin_start_rect);
-	text_layer_set_text_color(text_pin_layer, GColorWhite);
 	text_layer_set_background_color(text_pin_layer, GColorClear);
 	text_layer_set_text_alignment(text_pin_layer, GTextAlignmentCenter);
 	font_BITWISE_32 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_BITWISE_32));
 	text_layer_set_font(text_pin_layer, font_BITWISE_32);
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_pin_layer));
 	
-	window_set_click_config_provider(window, (ClickConfigProvider) window_config_provider);
 	tick_timer_service_subscribe(SECOND_UNIT, &handle_second_tick);
 	
+	set_theme();
 	window_stack_push(window, true /* Animated */);
 	
 	app_message_register_inbox_received(in_received_handler);
@@ -349,13 +448,6 @@ void handle_init(void) {
 	
 	image_icon_yes = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ICON_YES);
 	image_icon_no = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ICON_NO);
-	
-	// Initialize the action bar:
-	action_bar_layer = action_bar_layer_create();
-	action_bar_layer_set_background_color(action_bar_layer, GColorWhite);
-	action_bar_layer_set_click_config_provider(action_bar_layer, actionbar_config_provider);
-	action_bar_layer_set_icon(action_bar_layer, BUTTON_ID_UP, image_icon_yes);
-    action_bar_layer_set_icon(action_bar_layer, BUTTON_ID_DOWN, image_icon_no);
 }
 
 void handle_deinit(void) {
